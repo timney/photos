7756d8b8b361ca99b9b73ad0509f9cb751b80dc7
Andrew Timney <andrew.timney@gmail.com>
2016-03-24 12:35:30 +0000

    Initial commit

---
+++ timney_photos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="timney_photos.MvcApplication" Language="C#" %>
---
+++ timney_photos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(timney_photos.Startup))]
+namespace timney_photos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
