932b7477642593282de506b1ac41d4fa56afb033
Andrew Timney <andrew.timney@gmail.com>
2016-04-13 15:30:03 +0100

    Add album page

--- timney_photos/Controllers/HomeController.cs
+++ timney_photos/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace timney_photos.Controllers
 {
@@ -12,6 +16,19 @@ namespace timney_photos.Controllers
         public ActionResult Random()
         {
             return View();
+        }
+
+        public ActionResult Album()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("thumbnail");
+
+            var count = container.ListBlobs().Count();
+
+            return View(count);
         }
     }
 }
